e8c270f4d6babe739993a4535d334adc26d94955
jrt-PC <johanngruenh@gmail.com>
2015-09-17 12:01:43 +0200

    Implementiert mitscrollende TextBox und Erzeugung und Schreiben in eine txt-Datei

---
+++ Test__txt_Kommunikation/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Test__txt_Kommunikation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(100, 37);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "test";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "write txt-file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Test__txt_Kommunikation/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test__txt_Kommunikation
+{
+    public partial class Form1 : Form
+    {
+
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string newText = string.Format(Environment.NewLine + "test {0}", count.ToString());
+            textBox1.Text += newText;
+            count++;
+            textBox1.SelectionStart = textBox1.TextLength;
+            textBox1.ScrollToCaret();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.File.Exists(@"D:\Git-Repos\Test__txt_Kommunikation\Test__txt_Kommunikation\txt-test\test1.txt"))
+            {
+                System.IO.File.Create(@"D:\Git-Repos\Test__txt_Kommunikation\Test__txt_Kommunikation\txt-test\test1.txt");
+            }
+               
+            System.IO.File.WriteAllText(@"D:\Git-Repos\Test__txt_Kommunikation\Test__txt_Kommunikation\txt-test\test1.txt", textBox1.Text);
+        }
+    }
+}
